08da3b3534812f862048dee28784940022a7e357
vasv1 <vasv1@bk.ru>
2025-10-19 18:32:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@
 from flask import Flask, request
 app = Flask(__name__)
 @app.route('/')
+
 def hello_world():
     # Потенциальная уязвимость: кривое преобразование данных от пользователя.
     # Это просто для демонстрации, Bandit может проверить и такие вещи.
@@ -12,6 +13,7 @@ def hello_world():
         user_id = int(request.args.get('id', 1))
     except ValueError:
         user_id = 1
-        return f'<h1>Hello, user #{user_id}!</h1>'
-    if __name__ == '__main__':
-        app.run(debug=True) # Уязвимость! Никогда не используйте debug=True в продакшене.
+    return f'<h1>Hello, user #{user_id}!</h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True) # Уязвимость! Никогда не используйте debug=True в продакшене.
