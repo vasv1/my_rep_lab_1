6e81501e4cb29225a8a1c6a55c204d4257129517
vasv1 <vasv1@bk.ru>
2025-10-19 18:23:45 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+﻿# app.py
+#import sys
+#sys.path.append('c:/users/vit/appdata/local/programs/python/python37-32/Scripts')
+
+from flask import Flask, request
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    # Потенциальная уязвимость: кривое преобразование данных от пользователя.
+    # Это просто для демонстрации, Bandit может проверить и такие вещи.
+    try:
+        user_id = int(request.args.get('id', 1))
+    except ValueError:
+        user_id = 1
+        return f'<h1>Hello, user #{user_id}!</h1>'
+    if __name__ == '__main__':
+        app.run(debug=True) # Уязвимость! Никогда не используйте debug=True в продакшене.
+
+print("Готово")
