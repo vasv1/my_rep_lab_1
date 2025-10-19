a501af309deb57f05fc41805707e297b0a9d4ac1
vasv1 <vasv1@bk.ru>
2025-10-19 18:09:46 +0300

    Delete test_app.py.py

--- test/test_app.py.py
+++
@@ -1,9 +0,0 @@
-﻿# tests/test_app.py 
-import pytest 
-from app import app 
-
-def test_hello_world(): 
-    with app.test_client() as client: 
-        response = client.get('/') 
-        assert response.status_code == 200 
-        assert b'Hello' in response.data
