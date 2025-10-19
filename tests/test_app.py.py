5330677b550a4877c32b9c5823927fd3416286bb
vasv1 <vasv1@bk.ru>
2025-10-19 18:09:43 +0300

    Create test_app.py.py

---
+++ tests/test_app.py.py
@@ -0,0 +1,9 @@
+﻿# tests/test_app.py 
+import pytest 
+from app import app 
+
+def test_hello_world(): 
+    with app.test_client() as client: 
+        response = client.get('/') 
+        assert response.status_code == 200 
+        assert b'Hello' in response.data
